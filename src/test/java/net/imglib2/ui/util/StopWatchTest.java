ec18de4fa292c6ab7c93e58aa6cf53d0f6bea40f
Curtis Rueden <ctrueden@wisc.edu>
2014-10-20 15:24:28 -0500

    Add a simple unit test
    This fixes the Jenkins build, which expects there to be at least one
    unit test.

---
+++ src/test/java/net/imglib2/ui/util/StopWatchTest.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2014 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
+ * Stephan Saalfeld, Albert Cardona, Curtis Rueden, Christian Dietz, Jean-Yves
+ * Tinevez, Johannes Schindelin, Lee Kamentsky, Larry Lindsey, Grant Harris,
+ * Mark Hiner, Aivar Grislis, Martin Horn, Nick Perry, Michael Zinsmaier,
+ * Steffen Jaensch, Jan Funke, Mark Longair, and Dimiter Prodanov.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.ui.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link StopWatch}.
+ * 
+ * @author Curtis Rueden
+ */
+public class StopWatchTest
+{
+
+	@Test
+	public void testStopWatch()
+	{
+		final StopWatch stopWatch = new StopWatch();
+		final long time1 = stopWatch.nanoTime();
+		final long time2 = stopWatch.nanoTime();
+		stopWatch.start();
+		final long time3 = stopWatch.nanoTime();
+		final long time4 = stopWatch.nanoTime();
+		stopWatch.stop();
+		final long time5 = stopWatch.nanoTime();
+		final long time6 = stopWatch.nanoTime();
+		stopWatch.start();
+		final long time7 = stopWatch.nanoTime();
+		final long time8 = stopWatch.nanoTime();
+		stopWatch.stop();
+		final long time9 = stopWatch.nanoTime();
+		final long time10 = stopWatch.nanoTime();
+
+		assertEquals(time1, time2);
+		assertTrue(time3 >= time2);
+		assertTrue(time4 >= time3);
+		assertTrue(time5 >= time4);
+		assertEquals(time5, time6);
+		assertTrue(time7 >= time6);
+		assertTrue(time8 >= time7);
+		assertTrue(time9 >= time8);
+		assertEquals(time9, time10);
+	}
+
+}
